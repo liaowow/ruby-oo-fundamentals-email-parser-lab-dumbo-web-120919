5f4a5c45a17866a434acc50403f561efd649ecf4
Annie Liao <liaowow@yahoo.com>
2019-12-10 19:07:50 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,6 +12,6 @@ class EmailAddressParser
   attr_accessor :email_addresses
 
   def parse
-    email_addresses = @email_addresses.split(/,| /).uniq
+    email_addresses = @email_addresses.split(/, | /).uniq
   end
 end
