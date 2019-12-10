35dee86291de9491cae85970c8c7091859795d0d
Annie Liao <liaowow@yahoo.com>
2019-12-10 16:41:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailAddressParser(email_addresses)
+class EmailAddressParser
 
   attr_accessor :email_addresses
 
